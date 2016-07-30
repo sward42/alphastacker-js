2d5247d1a43403ebfa978623b93514d4010260d7
Sarah Ward <sarahward@Sarahs-MacBook-Air.local>
2016-07-30 13:42:13 -0500

    alpha stacker

---
+++ alphastacker.js
@@ -0,0 +1,43 @@
+/*  Create a stackLetters function that should console.log rows of letters. It should take an array containing the letters of the alphabet and output them like this:
+
+a
+ab
+abc
+abcd
+abcde
+abcdef
+etc
+Then add logic to stackLetters that places a space after every fifth letter, like this:
+
+a
+ab
+abd
+abcd
+abcde
+abcde f
+etc
+
+*/
+
+var alpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var alphaAdd = "";
+var stacker = "";
+
+
+for (var i = 0; i < alpha.length; i++) {
+	alphaAdd = alpha.slice(0, i);
+	stacker = alphaAdd.toString();
+	console.log(stacker);
+}
+
+console.log(alpha.splice(4, 0, ""));
+
+
+	
+
+/*
+	console.log(alphaAdd = alpha[0]);
+	console.log(alphaAdd += alpha[1]);
+	console.log(alphaAdd += alpha[2]);
+	console.log(alphaAdd += alpha[3]);
+*/
